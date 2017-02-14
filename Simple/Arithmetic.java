ca547e702d979648f41c6bf2a66f83c5dc8326e5
kaustabpal <kaustabpal22@gmail.com>
2017-02-14 19:33:46 +0530

    Create Arithmetic.java

---
+++ Simple/Arithmetic.java
@@ -0,0 +1,22 @@
+// Simple java program to demonstrate the basic Arithmetic operations
+//Author: Kaustab Pal
+//Date: 14/02/2016
+
+class Arithmetic
+{
+public static void main(String[] args)
+{
+int a,b,c,d;
+a=10;
+b=20;
+c=30;
+d=a+b+c;
+System.out.println("The sum of "+a+", "+b+" and "+c+" is: "+d);
+d=a*b*c;
+System.out.println("The product of "+a+", "+b+" and "+c+" is: "+d);
+d=c/b;
+System.out.println("The division of "+c+" and "+b+" is: "+d);
+d=c-b;
+System.out.println("The difference between "+c+" and "+b+" is: "+d);
+}
+}
